9a1aab49f9d269b3044382b9b6edbdb9289211aa
unknown <esfortnitebr@gmail.com>
2020-07-23 21:05:28 +0200

    fix

--- motd.py
+++ motd.py
@@ -11,6 +11,7 @@ auth.set_access_token(environ["KEY"], environ["SECRET"])
 response = requests.get(environ["URL"])
 MOTDs = response.json()['data']
 status = response.json()['status']
+tweetMOTD = 'false'
 
 setDelay = 120
 
@@ -21,24 +22,32 @@ while 1:
         print('Buscando cambios...')
     response = requests.get('https://fortnite-api.com/v2/news/br?language=es')
     MOTDLoop = response.json()['data']
-    if MOTDs != MOTDLoop:
+    if MOTDs == MOTDLoop:
         print('Se han detectado cambios...')
         for i in MOTDLoop['motds']:
             try:
-                print('Se ha detectado' +i['id'])
+                print('Se ha detectado ' +i['id'])
                 url = i['image']
                 r = requests.get(url, allow_redirects=True)
                 open(i['id']+'.png', 'wb').write(r.content)
-                print('Se ha guardado correctamente' +['id']+'.png')
-                try:
-                    api = tweepy.API(auth)
-                    api.update_with_media('Images/'+i['id']+'.png', 'MOTD:\n\n'+i['title']+'\n'+i['body'])
-                    print('Se ha publicado en Twitter:' +i['id'])
-                except:
-                    print('Ha habido un error al publicar el Tweet.')
+                print('Se ha guardado correctamente ' +i['id']+'.png')
+                if tweetMOTD == 'true':
+                    for i in MOTDLoop['motds']: 
+                        try:
+                            api = tweepy.API(auth)
+                            api.update_with_media(i['id']+'.png', i['title']+'\n\n'+i['body'])
+                            print('Se ha publicado en Twitter: ' +i['id'])
+                            try:
+                                time.sleep(3)
+                                os.remove(i['id']+'.png')
+                                MOTDs = response.json()['data']
+                            except:
+                                print('Error al eliminar las imágenes 60 segundos después de ser publicadas.')
+                        except:
+                            print('Ha habido un error al publicar el Tweet.')
             except:
                 print('Error al guardar la imagen.')
     else:
         print('No se detectan cambios.')
-    time.sleep(setDelay)
 
+    time.sleep(setDelay)
